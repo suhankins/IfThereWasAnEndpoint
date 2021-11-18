1894dfe2dfd8634d11f6cbdaa73cb2a2a9904bd8
ObiArt <punishedbernadetta@niuitmo.ru>
2021-11-18 21:02:22 +0300

    crosses are now out of sync and change color

--- scripts/index.js
+++ scripts/index.js
@@ -10,26 +10,24 @@ function randomAngle(min, max) {
     return Math.round(Math.random() * (max - min) + min) + "deg"
 }
 
-function repositionCross() {
-    for (let i = 0; i < crosses.length; i++) {
-
-        if (Math.random() < 0.1) {
-            if (crosses[i].style.display == "block") {
-                crosses[i].src = "images/cross_" + Math.floor(Math.random() * 4) + ".svg"
-                continue
-            }
-        } else {
-            crosses[i].style.display = "none"
+function repositionCross(i) {
+    if (Math.random() < 0.1) {
+        if (crosses[i].style.display == "block") {
+            crosses[i].src = "images/cross_" + Math.floor(Math.random() * 4) + ".svg"
+            return
         }
-
-        crosses[i].style.display = "block"
-        crosses[i].style.top = randomPosition(-25, 100)
-        crosses[i].style.left = randomPosition(-25, 100)
-        var size = randomPosition(5, 20)
-        crosses[i].style.width = size
-        crosses[i].style.height = size
-        crosses[i].style.transform = "rotate(" + randomAngle(-10, 10) + ")"
+    } else {
+        crosses[i].style.display = "none"
+        crosses[i].setAttribute("class", "cross color" + Math.floor(Math.random() * 4))
     }
+
+    crosses[i].style.display = "block"
+    crosses[i].style.top = randomPosition(-25, 100)
+    crosses[i].style.left = randomPosition(-25, 100)
+    var size = randomPosition(5, 20)
+    crosses[i].style.width = size
+    crosses[i].style.height = size
+    crosses[i].style.transform = "rotate(" + randomAngle(-10, 10) + ")"
 }
 
 function repositionRectangle() {
@@ -54,12 +52,14 @@ function repositionDot() {
         }
         dots[i].style.top = randomPosition(-25, 100)
         dots[i].style.left = randomPosition(-25, 100)
-        dots[i].style.width = randomPosition(0.25, 0.5)
-        dots[i].style.height = randomPosition(0.25, 0.5)
+        dots[i].style.width = randomPosition(0.3, 0.5)
+        dots[i].style.height = randomPosition(0.3, 0.5)
         dots[i].style.transform = "rotate(" + randomAngle(-180, 180) + ")"
     }
 }
 
-setInterval(repositionCross, 120);
+for (let i = 0; i < crosses.length; i++) {
+    setInterval(function() { repositionCross(i) }, 120 - (Math.floor(Math.random() * 40 - 20)));
+}
 setInterval(repositionRectangle, 100);
 setInterval(repositionDot, 150);
